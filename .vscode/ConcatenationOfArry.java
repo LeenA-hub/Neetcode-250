cdf3415edb466b277b73b3b25c1c28e770e50145
leen128 <141294462+leen128@users.noreply.github.com>
2025-09-12 23:11:06 -0400

    default

--- .vscode/ConcatenationOfArry.java
+++ .vscode/ConcatenationOfArry.java
@@ -1,14 +1,5 @@
 class Solution {
-    public boolean hasDuplicate(int[] nums) {
+    public int[] getConcatenation(int[] nums) {
 
-    }}
-
-    Set<Integer> set = new HashSet<>();for(
-    int num:nums)
-    {
-            if (set.contains(num)) {
-                return true; // Duplicate found
-            }
-            set.add(num);
-        }return false;
-// No duplicates found
+    }
+}
