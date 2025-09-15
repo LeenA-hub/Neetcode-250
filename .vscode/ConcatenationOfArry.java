82870439177e07b763eeb82e842dff60da3798f0
LeenA-hub <almutileen7@gmail.com>
2025-09-14 22:31:13 -0400

    a

--- .vscode/ConcatenationOfArry.java
+++
@@ -1,7 +0,0 @@
-//need to create new ans of length 2*n, so beasically want to return the nums array content twice into new arrya 
-
-class Solution {
-    public int[] getConcatenation(int[] nums) {
-
-    }
-}
---
+++ .vscode/Solution.java
@@ -0,0 +1,11 @@
+//need to create new ans of length 2*n, so beasically want to return the nums array content twice into new arrya 
+
+class Solution {
+  public int[] getConcatenation(int[] nums) {
+      int ans [];
+      if (ans[i] == nums [i]){
+        n = 2*n;
+      }
+      return ans;
+
+}
