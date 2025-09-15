f90c9bfc684834de497514a057c5de6844bd03ef
LeenA-hub <almutileen7@gmail.com>
2025-09-14 22:21:32 -0400

    added instruction

--- .vscode/ConcatenationOfArry.java
+++ .vscode/ConcatenationOfArry.java
@@ -1,5 +1,7 @@
+//need to create new ans of length 2*n, so beasically want to return the nums array content twice into new arrya 
+
 class Solution {
-    public boolean hasDuplicate(int[] nums) {
+    public int[] getConcatenation(int[] nums) {
 
     }
 }
