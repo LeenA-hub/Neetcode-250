cd2666758d1bfd2b97e8bb27e60b135c63af9115
LeenA-hub <almutileen7@gmail.com>
2025-09-22 23:34:12 -0400

    lol4

--- .vscode/Solution.java
+++ .vscode/Solution.java
@@ -68,6 +68,8 @@ for (int i = 0; i < nums.length; i++){
                  if (nums[i] + nums[j] == target) {
                 return new int[] {i, j};
               
-
-}
             }
+
+            }
+                return new int[] {};
+}
