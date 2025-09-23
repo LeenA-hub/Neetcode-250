7c8b7c3219a260c13044f0108fee588c40b40b14
LeenA-hub <almutileen7@gmail.com>
2025-09-22 23:32:06 -0400

    lol2

--- .vscode/Solution.java
+++ .vscode/Solution.java
@@ -60,9 +60,9 @@ public boolean isAnagram(String s, String t) {
   // we doing problem 4 on sep 20,2025
   // two sum problem
 public int[] twoSum(int[] nums, int target) {
-
-       for (int i = 0; i <= nums.length; i++){
-            for (int j = i+ 1; j <=0; j--){
+for (int i = 0; i < nums.length; i++){
+            for (int j = i+ 1; j < nums.length; j++){
+            
             
                
                 nums[i] + nums[j] = target;
